d1d757832d84ffdfb46bdee8834ff35a5135c481
i-iz-adam <96274526+i-iz-adam@users.noreply.github.com>
2026-09-15 15:39:09 +0100

    Bump version to 1.0.3

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -10,7 +10,7 @@ export default function About() {
   const { result: updateResult } = useUpdaterStore();
   const openLink = (url: string) => open(url);
 
-  const currentVersion = updateResult?.current_version || "1.0.2";
+  const currentVersion = updateResult?.current_version || "1.0.3";
 
   return (
     <div className="settings-view">
